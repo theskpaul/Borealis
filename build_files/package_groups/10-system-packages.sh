5aebe7ab9512b584f27afb79d81881152e8bb079
Sourav Kumar Paul <theskpaul@proton.me>
2026-06-05 18:59:02 +0530

    Update 10-system-packages.sh

--- build_files/package_groups/10-system-packages.sh
+++ build_files/package_groups/10-system-packages.sh
@@ -20,4 +20,4 @@ dnf5 --setopt=install_weak_deps=False install -y \
 # dnf copr enable -y bieszczaders/kernel-cachyos-addons
 # dnf install ananicy-cpp -y
 
-dnf5 config-manager setopt copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons.enabled=0
+# dnf5 config-manager setopt copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons.enabled=0
